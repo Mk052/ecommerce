f5a9413fe98bd1bc89655b7aea25d63885cdad2c
MK052 <madhukush006@gmail.com>
2025-04-11 11:23:51 +0530

    Added the Order and OrderItem models

--- eshop/models.py
+++ eshop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from eshop.manager import CustomUserManager
+from .manager import CustomUserManager
 from django.utils.timezone import now
 
 
@@ -85,3 +85,61 @@ class CartItem(TimestampedModel):
     
     def __str__(self):
         return f"{self.cart.user.email}, {self.product.title}"
+    
+    def total_price(self):
+        return self.product.price * self.quantity
+    
+class Billing_Address(TimestampedModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Link to user
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=20)
+    address_line = models.CharField(max_length=255)
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    pin_code = models.CharField(max_length=6)
+    is_shipping = models.BooleanField(default=False)  # Whether this is a shipping address
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.city}, {self.country}"
+
+# Order Model
+class Order(TimestampedModel):
+    STATUS_CHOICES = [
+        ("Pending", "Pending"),
+        ("Shipped", "Shipped"),
+        ("Delivered", "Delivered"),
+        ("Cancelled", "Cancelled"),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(Billing_Address, on_delete=models.SET_NULL, null=True, blank=True)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default="Pending")
+
+    def __str__(self):
+        return f"Order {self.id} - {self.user} - {self.status}"
+
+    # def calculate_total(self):
+    #     total = sum(item.price * item.quantity for item in self.items.all())
+    #     self.total_price = total
+    #     self.save()
+        
+    def shipping_charges(self):
+        return self.total_price + 40
+
+
+class OrderItem(TimestampedModel):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
+    product = models.CharField(max_length=255)
+    product_image = models.URLField()  
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.product} ({self.quantity})"
+
+    
+
+
